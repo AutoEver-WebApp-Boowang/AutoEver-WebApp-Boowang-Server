bb484a9daab8f7c06044287ae6b904267eadfcd9
dasolsee <dasol_2@naver.com>
2026-09-11 14:30:17 +0900

    setting

---
+++ src/main/java/com/example/boowang/BoowangApplication.java
@@ -0,0 +1,13 @@
+package com.example.boowang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoowangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoowangApplication.class, args);
+    }
+
+}
