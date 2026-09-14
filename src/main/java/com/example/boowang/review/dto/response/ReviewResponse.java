24c8f359e65e1014673a03dc387421b56542ef14
Baeksnagjun <juny200@naver.com>
2026-09-14 09:27:06 +0900

    feat: 리뷰응답 dto

---
+++ src/main/java/com/example/boowang/review/dto/response/ReviewResponse.java
@@ -0,0 +1,17 @@
+package com.example.boowang.review.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ReviewResponse {
+    private Long id;
+    private String content;
+    private Long likeCount;
+    private LocalDateTime createdAt;
+    private Long totalCount;
+}
