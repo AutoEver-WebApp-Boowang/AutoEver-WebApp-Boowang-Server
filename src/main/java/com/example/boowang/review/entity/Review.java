04e791dd6049e6379d0a2a3cfca7c52550ac9f66
Baeksnagjun <juny200@naver.com>
2026-09-14 18:05:32 +0900

    feat: ec2연결 도커파일 추가

--- src/main/java/com/example/boowang/review/entity/Review.java
+++ src/main/java/com/example/boowang/review/entity/Review.java
@@ -2,28 +2,28 @@ package com.example.boowang.review.entity;
 
 
 import jakarta.persistence.*;
-        import lombok.Getter;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
+@Table(name = "reviews")
 @Setter
+@Getter
 public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
     @Column(name = "place_id", nullable = false)
     private Long placeId;
 
-    @Column(name = "user_id", nullable = false)
-    private  Long userId;
-
-
-    @Column(columnDefinition = "TEXT")
+    @Column(name = "content")
     private String content;
 
     @Column(name = "created_at", nullable = false)
@@ -31,7 +31,6 @@ public class Review {
 
     @PrePersist
     protected void onCreate() {
-        createdAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now();
     }
-
 }
