accf061a6273926d3d4e5b50e7da930130e3e246
yunseo-1 <yunseo1229@naver.com>
2026-09-21 11:14:36 +0900

    feat: 장소 추천/비추천 API 추가

--- src/main/java/com/example/boowang/place/repository/PlaceReactionRepository.java
+++ src/main/java/com/example/boowang/place/repository/PlaceReactionRepository.java
@@ -4,7 +4,11 @@ import com.example.boowang.place.entity.PlaceReaction;
 import com.example.boowang.place.entity.PlaceReactionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlaceReactionRepository extends JpaRepository<PlaceReaction, PlaceReactionId> {
 
     long countByPlaceIdAndReactionType(Long placeId, String reactionType);
+    Optional<PlaceReaction> findByUserIdAndPlaceId(Long userId, Long placeId);
+    void deleteByUserIdAndPlaceId(Long userId, Long placeId);
 }
